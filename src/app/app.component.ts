76b930218f605af4c502e576d6d91f045945f1ab
sohail-ahamed-qureshi <sohailqureshi82@gmail.com>
2021-05-22 18:30:08 +0530

    Frist AngularJS project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cwh-todo-list';
+  constructor(){
+    // setTimeout(() => {
+    //   this.title = "Todo List";
+    // },2000);
+ 
+
+  }
 }
